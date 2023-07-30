ebd4003420114d4c9185fb0bb03a0d3e53dae822
outstandyy <zinchenko.vladislav.1995@gmail.com>
2023-07-30 19:24:18 +0200

    add abstract view

---
+++ README.md
@@ -0,0 +1,5 @@
+## Implement my own SPA without frameworks
+
+
+#### Resources:
+- https://www.youtube.com/watch?v=6BozpmSjk-Y&ab_channel=dcode
--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,1 +1,46 @@
 console.log('JS is loaded.')
+
+const navigateTo = url => {
+	history.pushState(null, null, url);
+	router();
+}
+
+const router = async () => {
+	const routes = [
+		{ path: '/', view: () => console.log('Viewing dashboard...') },
+		{ path: '/posts', view: () => console.log('Viewing posts...') },
+		{ path: '/settings', view: () => console.log('Viewing settings...') },
+	];
+
+	// Test each route for potential match
+	const potentialMatches = routes.map(route => {
+			return {
+				route: route,
+				isMatch: location.pathname === route.path,
+			}
+		}
+	);
+
+	let match = potentialMatches.find(potentialMatch => potentialMatch.isMatch);
+
+	if (!match) {
+		match = {
+			route: routes[0],
+			isMatch: true,
+		}
+	}
+
+	console.log(match.route.view());
+};
+
+window.addEventListener('popstate', router);
+
+document.addEventListener('DOMContentLoaded', () => {
+	document.body.addEventListener('click', e => {
+		if (e.target.matches('[data-link]')) {
+			e.preventDefault();
+			navigateTo(e.target.href);
+		}
+	});
+	router();
+});
